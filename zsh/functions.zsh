851b33b3cb3f63df9f95e94dd34125317666d717
alonalon <aronhafner@gmail.com>
2017-08-09 21:36:39 +0200

    Add new functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,7 +6,24 @@ md() {
 }
 
 # List globally installed npm packages
-
 npmls() {
 	npm ls -gp | awk -F/ '/node_modules/ && !/node_modules.*node_modules/ {print $NF}'
+}
+
+# `s` with no arguments opens the current directory in Sublime Text, otherwise opens the given location
+function s() {
+	if [ $# -eq 0 ]; then
+		subl .;
+	else
+		subl "$@";
+	fi;
+}
+
+# `o` with no arguments opens the current directory, otherwise opens the given location
+function o() {
+	if [ $# -eq 0 ]; then
+		open .;
+	else
+		open "$@";
+	fi;
 }
