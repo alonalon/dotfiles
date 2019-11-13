8b781271c778023b80d6efebc9607996832cb7f6
Aron Hafner <aronhafner@gmail.com>
2019-11-14 00:43:36 +0100

    Dotbot (#4)
    * Use dotbot
    
    * Change code defaults
    
    * Add shorthand function for open path in vscode
    
    * code settings
    
    * Add hyper settings

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,9 +1,36 @@
 # General
+# Editor
+export EDITOR="code --wait"
 
-export EDITOR='atom'         # Editor
+# Path to n
+export N_PREFIX="${HOME}/.n"
 
-# Locale
+# Path to npm packages
+export NPM_PACKAGES="${HOME}/.npm-packages"
 
+# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
+unset MANPATH
+
+# Add `~/.npm-packages/share/man` to the `$MANPATH`
+export MANPATH="${NPM_PACKAGES}/share/man:$(manpath)"
+
+# Prefer US English and use UTF-8
 export LC_ALL="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 export LANGUAGE="en_US.UTF-8"
+
+# Prepend paths
+declare -a DIRS=(
+	"/usr/local/bin"
+	"/usr/local/sbin"
+	"${N_PREFIX}/bin"
+	"${NPM_PACKAGES}/bin"
+	"${HOME}/bin"
+)
+
+for x in "${DIRS[@]}"; do
+	[[ -d "$x" ]] && PATH="${x}:${PATH}"
+done
+
+unset DIRS
+export PATH
