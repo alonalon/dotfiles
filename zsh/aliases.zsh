0477ec520cd303180b19d21a516e0a4cdaa5bcc1
hafffe <aronhafner@gmail.com>
2020-08-24 21:46:04 +0200

    Move git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,8 +10,25 @@ alias la='ls -a'
 # Print each PATH entry on a separate line
 alias path='echo -e ${PATH//:/\\n}'
 
-#Reload alias
+# Reload alias
 alias reload!='. ~/.zshrc'
+
+# Git Aliases
+alias g="git"
+alias gb="git branch"
+alias gbr="git br"
+alias gc="git c"
+alias gca="git ca"
+alias gd="git d"
+alias gdi="git di"
+alias gl="git l"
+alias gls="git ls"
+alias go="git go"
+alias gp="git p"
+alias gpo="git po"
+alias gr="git gr"
+alias gra="git gra"
+alias gs="git s"
 
 # OSX specific aliases
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles TRUE;killall Finder'
